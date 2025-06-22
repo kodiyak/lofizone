8002a96cb8ce934ec171b596c980a2899111fab6
kodiyak <mathewsto51@gmail.com>
2025-06-22 07:22:35 -0300

    feat: enhance NavTop component with user session handling and dropdown menu; update authClient import

--- apps/web/src/app/(public)/components/nav-top.tsx
+++ apps/web/src/app/(public)/components/nav-top.tsx
@@ -1,8 +1,27 @@
+'use client';
+
+import { authClient } from '@/lib/authClient';
+import { CaretDownIcon } from '@phosphor-icons/react';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@workspace/ui/components/avatar';
 import { Button } from '@workspace/ui/components/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@workspace/ui/components/dropdown-menu';
+import { Skeleton } from '@workspace/ui/components/skeleton';
 import Link from 'next/link';
 import React from 'react';
 
 export default function NavTop() {
+  const { data: session, isPending } = authClient.useSession();
   return (
     <div className="fixed w-full z-50 left-0 top-0 h-[var(--nav-top-height)] border-b border-border/50 bg-background/80 backdrop-blur-lg">
       <div className="container mx-auto h-full flex items-center">
@@ -13,9 +32,47 @@ export default function NavTop() {
           <Button className="rounded-full" variant={'outline'}>
             Create Room
           </Button>
-          <Button className="rounded-full" asChild>
-            <Link href={'/login'}>Login</Link>
-          </Button>
+          {isPending ? (
+            <Skeleton className="w-[180px] h-10 rounded-sm" />
+          ) : (
+            <>
+              {session ? (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant={'ghost'} className="pl-2 rounded-full">
+                      <Avatar className="size-6">
+                        <AvatarImage src={session.user.image || undefined} />
+                        <AvatarFallback>
+                          {session.user.name?.charAt(0) || 'U'}
+                        </AvatarFallback>
+                      </Avatar>
+                      <div className="flex flex-col text-left items-start">
+                        <span>{session.user.email}</span>
+                      </div>
+                      <CaretDownIcon className="size-3!" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-[var(--radix-dropdown-menu-trigger-width)]">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem
+                      onSelect={(e) => {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        authClient.signOut();
+                      }}
+                    >
+                      Logout
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              ) : (
+                <Button className="rounded-full" asChild>
+                  <Link href={'/login'}>Login</Link>
+                </Button>
+              )}
+            </>
+          )}
         </div>
       </div>
     </div>
