e8ddd1bb703e20e07f8bff9fb6c36d9c2d8f8221
kodiyak <mathewsto51@gmail.com>
2025-06-26 01:06:19 -0300

    chore: remove unused imports and commented code from layout component

--- apps/web/src/app/(public)/layout.tsx
+++ apps/web/src/app/(public)/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from 'next';
 import { Geist as Font_Sans, Geist_Mono as Font_Mono } from 'next/font/google';
 import '@workspace/ui/globals.css';
 import { cn } from '@workspace/ui/lib/utils';
-import Wallpaper from '@/assets/images/wallpaper.png';
-import Image from 'next/image';
-import NavTop from './components/nav-top';
 import type { CSSProperties } from 'react';
 import { QueryProvider } from '@/components/providers/query-provider';
 import { AuthProvider } from '@/components/providers/auth-provider';
@@ -53,20 +50,10 @@ export default function RootLayout({
         <QueryProvider>
           <AuthProvider>
             <BackgroundProvider />
-            {/* <NavTop /> */}
             <div className="flex flex-col w-full h-screen overflow-hidden">
               <div className="flex flex-1 relative items-stretch">
                 <Sidebar />
                 <div className="flex-1 relative overflow-hidden">
-                  {/* <div className="absolute -z-10 w-[50vw] right-0 top-0 -translate-y-1/5 translate-x-1/2">
-                  <div className="size-full absolute left-0 top-0 bg-gradient-to-b from-transparent to-background"></div>
-                  <div className="size-full absolute left-0 top-0 bg-gradient-to-l from-transparent to-background"></div>
-                  <Image
-                    src={Wallpaper}
-                    alt={'Wallpaper Lofi'}
-                    className="w-full h-screen object-cover object-center"
-                  />
-                </div> */}
                   {children}
                 </div>
               </div>
