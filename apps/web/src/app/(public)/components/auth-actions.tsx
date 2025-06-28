8afa50e649bf64d7d42d05d030fa91c938717c3b
kodiyak <mathewsto51@gmail.com>
2025-06-28 00:58:53 -0300

    feat: implement AuthActions component for user authentication and integrate it into Sidebar; enhance DropdownMenu styles for better UI experience

--- apps/web/src/app/(public)/components/auth-actions.tsx
+++ apps/web/src/app/(public)/components/auth-actions.tsx
@@ -1,5 +1,43 @@
-import React from 'react';
+'use client';
 
-export default function AuthActions() {
-  return <div>AuthActions</div>;
+import React, { type PropsWithChildren } from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from '@workspace/ui/components/dropdown-menu';
+import type { User } from 'better-auth';
+import { LogOutIcon } from 'lucide-react';
+import { authClient } from '@/lib/authClient';
+
+export default function AuthActions({
+  children,
+  user,
+}: PropsWithChildren<{
+  user: User;
+}>) {
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+        <DropdownMenuContent className="w-[var(--radix-dropdown-menu-trigger-width)]">
+          <DropdownMenuLabel className="truncate">
+            {user.email}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator className="mb-2" />
+          <DropdownMenuItem
+            onClick={() => {
+              authClient.signOut();
+            }}
+          >
+            <span>Logout</span>
+            <LogOutIcon />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
 }
--- apps/web/src/app/(public)/components/sidebar.tsx
+++ apps/web/src/app/(public)/components/sidebar.tsx
@@ -7,8 +7,18 @@ import Link from 'next/link';
 import { Button } from '@workspace/ui/components/button';
 import { HomeIcon, LibraryIcon, SettingsIcon } from 'lucide-react';
 import { GlobeIcon } from '@phosphor-icons/react';
+import { authClient } from '@/lib/authClient';
+import { Skeleton } from '@workspace/ui/components/skeleton';
+import AuthActions from './auth-actions';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@workspace/ui/components/avatar';
 
 export default function Sidebar() {
+  const { data: session, isPending } = authClient.useSession();
+
   return (
     <>
       <div className="border-r w-[200] flex flex-col bg-background/80 backdrop-blur-xs">
@@ -22,7 +32,7 @@ export default function Sidebar() {
             />
           </Link>
         </div>
-        <div className="flex flex-col px-6">
+        <div className="flex flex-col items-start px-6">
           {[
             {
               name: 'Home',
@@ -45,19 +55,42 @@ export default function Sidebar() {
               icon: <SettingsIcon />,
             },
           ].map((item) => (
-            <Button
-              asChild
-              key={item.name}
-              variant={'ghost'}
-              className="justify-start"
-              size={'sm'}
-            >
-              <Link href={item.href}>
+            <Button asChild key={item.name} variant={'ghost'}>
+              <Link
+                href={item.href}
+                className="justify-start pl-4! pr-6! rounded-3xl"
+              >
                 {item.icon}
                 <span>{item.name}</span>
               </Link>
             </Button>
           ))}
+        </div>
+        <div className="flex-1"></div>
+        <div className="p-4">
+          {isPending ? (
+            <Skeleton className="h-10" />
+          ) : (
+            <>
+              {session ? (
+                <AuthActions user={session.user}>
+                  <Button className="w-full justify-start" variant={'ghost'}>
+                    <Avatar className="size-8">
+                      <AvatarImage src={session?.user?.image || undefined} />
+                      <AvatarFallback>
+                        {session?.user?.name?.charAt(0) || 'U'}
+                      </AvatarFallback>
+                    </Avatar>
+                    <span>{session?.user?.name}</span>
+                  </Button>
+                </AuthActions>
+              ) : (
+                <Button asChild variant={'outline'} className="w-full">
+                  <Link href={'/login'}>Login</Link>
+                </Button>
+              )}
+            </>
+          )}
         </div>
       </div>
     </>
